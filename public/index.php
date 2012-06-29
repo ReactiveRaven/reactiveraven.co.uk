cecb5443ff838458423ee7c63b7287125b27c894
Reactive Raven <reactiveraven@gmail.com>
2012-06-30 00:53:07 +0100

    updating with logo

--- public/index.php
+++ public/index.php
@@ -3,6 +3,47 @@ $created = strtotime("2012-05-22 21:30:00");
 $now = time();
 $daylength = 60*60*24;
 $timeago = round(($now - $created) / $daylength, 2);
-?>
-<h1>I registered this domain <?php echo $timeago; ?> days ago</h1>
-If that seems like a long time, bug me at <a href="http://www.twitter.com/ReactiveRaven">@reactiveraven</a> :)
+?><!DOCTYPE html>
+<html>
+  <head>
+    <title>ReactiveRaven.co.uk</title>
+    <style>
+      body
+      {
+        background: black;
+        color: white;
+      }
+      
+      a, a:visited, a:active
+      {
+        color: yellow;
+      }
+      
+      a:hover
+      {
+        color: white;
+      }
+      
+      .logo
+      {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 500px;
+        margin-left: -250px;
+        margin-top: -250px;
+      }
+    </style>
+  </head>
+  <body>
+    <img src="/images/rrlogowhite.svg" class="logo" />
+    <!-- 
+      much better, no?
+      A nice, oblique logo.
+    
+      by the way, if you want to contact me try:
+      twitter @reactiveraven
+      email   reactiveraven @gmail.com
+    -->
+  </body>
+</html>
