fd470610fc551abc048b23076906fb30a5d9768f
Reactive Raven <reactiveraven@gmail.com>
2012-06-30 00:43:28 +0100

    test commit

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ $daylength = 60*60*24;
 $timeago = round(($now - $created) / $daylength, 2);
 ?>
 <h1>I registered this domain <?php echo $timeago; ?> days ago</h1>
-If that seems like a long time, bug me at <a href="http://www.twitter.com/ReactiveRaven">@reactiveraven</a>
+If that seems like a long time, bug me at <a href="http://www.twitter.com/ReactiveRaven">@reactiveraven</a> :)
