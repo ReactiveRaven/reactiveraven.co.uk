5336e3f99cea1fb16770b8ae8c09ae836ced4bb7
David Godfrey <david.godfrey@coolpink.net>
2012-06-30 01:52:07 +0100

    fixing issue in receive.php

--- public/receive.php
+++ public/receive.php
@@ -2,6 +2,8 @@
 
 $postdata = file_get_contents("php://input");
 
-file_put_contents("output.txt", var_export($postdata), FILE_APPEND);
-file_put_contents("output.txt", var_export($_POST), FILE_APPEND);
-file_put_contents("output.txt", var_export($_SERVER), FILE_APPEND);
+file_put_contents("output.txt", "testing", FILE_APPEND);
+
+file_put_contents("output.txt", var_export($postdata, true), FILE_APPEND);
+file_put_contents("output.txt", var_export($_POST, true), FILE_APPEND);
+file_put_contents("output.txt", var_export($_SERVER, true), FILE_APPEND);
