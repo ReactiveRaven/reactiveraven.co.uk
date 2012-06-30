7e463d836781cd86290dc07a9de3a3a514736cc0
Reactive Raven <reactiveraven@gmail.com>
2012-06-30 02:02:16 +0100

    trimming down receive.php to bare minimum

--- public/receive.php
+++ public/receive.php
@@ -1,9 +1,3 @@
 <?php
 
-$postdata = file_get_contents("php://input");
-
-file_put_contents("output.txt", "testing", FILE_APPEND);
-
-file_put_contents("output.txt", var_export($postdata, true), FILE_APPEND);
 file_put_contents("output.txt", var_export($_POST, true), FILE_APPEND);
-file_put_contents("output.txt", var_export($_SERVER, true), FILE_APPEND);
