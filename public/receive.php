da8ca6acb94cb4fcd1172e8919e0fc848c31591f
Reactive Raven <reactiveraven@gmail.com>
2012-06-30 01:49:07 +0100

    testing

--- public/receive.php
+++ public/receive.php
@@ -4,3 +4,4 @@ $postdata = file_get_contents("php://input");
 
 file_put_contents("output.txt", var_export($postdata), FILE_APPEND);
 file_put_contents("output.txt", var_export($_POST), FILE_APPEND);
+file_put_contents("output.txt", var_export($_SERVER), FILE_APPEND);
