05f21dd16b9075dfb2266b3c10346dfc327b6c8d
Reactive Raven <reactiveraven@gmail.com>
2012-07-03 21:49:56 +0100

    initial add of new permission fix, rearrange of build scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./server/buildscripts/clear_cache.sh;
+./server/buildscripts/assetic_dump.sh;
+./server/buildscripts/permissions_fix.sh;
---
+++ server/buildscripts/permissions_fix.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+{
+
+echo;
+echo -n "PERMISSIONS_FIX.SH -- ";
+date;
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+user="`cat $DIR/vars/localuser`";
+group="`cat $DIR/vars/localgroup`";
+
+pushd "$DIR/../../";
+
+find . | grep -v "\.git" | while read line;
+do
+  chown -v $user:$group "$line";
+done;
+
+popd;
+
+echo -n "END PERMISSIONS_FIX.SH -- ";
+date;
+
+} >> update.log;
