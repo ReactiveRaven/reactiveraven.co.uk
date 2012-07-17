29ed60dc7e719fa9c1bbe50033f8a31f2a731c48
Reactive Raven <reactiveraven@gmail.com>
2012-07-17 21:10:56 +0100

    updating build.sh to take into account things already done by the builder

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-php ./server/buildscripts/install_files.php &&
 ./server/buildscripts/clear_cache.sh &&
-./server/buildscripts/assetic_dump.sh &&
-./server/buildscripts/permissions_fix.sh
+./server/buildscripts/assetic_dump.sh
 
 exit $?;
